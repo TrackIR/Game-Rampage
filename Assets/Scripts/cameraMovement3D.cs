e30cf1541678533df50f6e19d06e0013a335ddd0
Ryan Waltemate <waltemry@oregonstate.edu>
2025-11-05 16:44:12 -0800

    added 3rd person toggle

--- Assets/Scripts/cameraMovement3D.cs
+++ Assets/Scripts/cameraMovement3D.cs
@@ -6,20 +6,26 @@ using UnityEngine;
 
 public class cameraMovement3D : MonoBehaviour
 {
-    public bool is3rdPerson = true;
 
+    public bool is3rdPerson = true;
     public GameObject playerObject;
     public GameObject cameraObject;
 
+    [Header("3rd Person Settings")]
     public float distance = 5;
     public float defaultHight = 8;
     public float horizontalOffectWeight = 20;
     public float verticalOffectWeight = 20;
     public float depthOffectWeight = 25;
 
+    [Header("1st Person Settings")]
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
 
     // moves the camera target's empty
-    void Move3DTargetPosition()
+    void Move3rdCamTargetPosition()
     {
         // camera's rotation
         Vector3 camRot = new Vector3(cameraObject.transform.rotation.x,
@@ -35,20 +41,29 @@ public class cameraMovement3D : MonoBehaviour
 
         // y pos change with up/down head movement
         targetPos.y = basePos.y + (camRot.x * verticalOffectWeight);
-        
+
         Math.Clamp(targetPos.y, 1f, math.INFINITY); // clamp targetPos.y so it never goes below ground level; scretch goal to cast a ray down so it never goes below ground/debris
 
         // z pos change tied to up/down head movement
         targetPos.z = basePos.z + (camRot.x * depthOffectWeight);
-        
+
         // set empty's position to target position
         transform.localPosition = targetPos;
+    }
+
+    void Move1stCamTargetPosition()
+    {
+        Vector3 targetPos = new Vector3(xOffset, yOffset, zOffset);
+        transform.localPosition = playerObject.transform.position + targetPos;
     }
 
     void Update()
     {
         if (is3rdPerson) {
-            Move3DTargetPosition();
+            Move3rdCamTargetPosition();
+        } else
+        {
+            Move1stCamTargetPosition();
         }
     }
 }
