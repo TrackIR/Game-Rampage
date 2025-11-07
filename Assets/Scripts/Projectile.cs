4643730640b81fe1289903bb588b9d2747323e40
Pranav Kulkarni <pranav.kulkarni.004@gmail.com>
2025-11-06 22:35:09 -0800

    Added Basic Enemy Behavior & Projectile Behavior
    - Enemy will shoot when in range
    - Enemy will face the player while in range

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+     // Basic Settings
+     public float speed = 75f;
+     public float lifeTime = 5f; // How long it lives before auto-destroying
+
+     void Start()
+     {
+          // 1. Get the Rigidbody component
+          Rigidbody rb = GetComponent<Rigidbody>();
+
+          // 2. Make it move forward
+          if (rb != null)
+          {
+               rb.linearVelocity = transform.forward * speed;
+          }
+
+          // 3. Set it to destroy itself after 'lifeTime' seconds
+          Destroy(gameObject, lifeTime);
+     }
+
+     void OnCollisionEnter(Collision collision)
+     {
+
+          // TODO: Check if player was hit
+          // if (collision.gameObject.CompareTag("Player"))
+          // {
+          //     // TODO: Add damage logic here
+          // }
+
+          // 4. Destroy the projectile on impact with anything (building or player)
+          Destroy(gameObject);
+     }
+}
