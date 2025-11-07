65606d416a2cf4efabfc28dc10902e743f10687b
Bopittostart <qtroth2004@gmail.com>
2025-11-07 11:05:07 -0800

    camera rotation now based on TrackIR rotation data instead of camera transform rotation. Fixes issue when camera rotates to follow player rotation camera x position goes off center.

--- Assets/Scripts/cameraMovement3D.cs
+++ Assets/Scripts/cameraMovement3D.cs
@@ -10,6 +10,9 @@ public class cameraMovement3D : MonoBehaviour
 
     public GameObject playerObject;
     public GameObject cameraObject;
+    public GameObject trackIRRoot;
+    
+    TrackIRComponent trackIR;
 
     public float distance = 5;
     public float defaultHight = 8;
@@ -17,14 +20,17 @@ public class cameraMovement3D : MonoBehaviour
     public float verticalOffectWeight = 20;
     public float depthOffectWeight = 25;
 
+    Quaternion headRot;
+
 
     // moves the camera target's empty
     void Move3DTargetPosition()
     {
+
+        headRot = trackIR.LatestPoseOrientation;
+
         // camera's rotation
-        Vector3 camRot = new Vector3(cameraObject.transform.rotation.x,
-                                     cameraObject.transform.rotation.y,
-                                     cameraObject.transform.rotation.z);
+        Vector3 camRot = new Vector3(headRot.x, headRot.y, headRot.z);
 
         Vector3 basePos = new Vector3(0f, defaultHight, distance);  // default pos for camera
         Vector3 targetPos = basePos;                                // target pos for camera
@@ -35,14 +41,19 @@ public class cameraMovement3D : MonoBehaviour
 
         // y pos change with up/down head movement
         targetPos.y = basePos.y + (camRot.x * verticalOffectWeight);
-        
+
         Math.Clamp(targetPos.y, 1f, math.INFINITY); // clamp targetPos.y so it never goes below ground level; scretch goal to cast a ray down so it never goes below ground/debris
 
         // z pos change tied to up/down head movement
         targetPos.z = basePos.z + (camRot.x * depthOffectWeight);
-        
+
         // set empty's position to target position
         transform.localPosition = targetPos;
+    }
+
+    void Start()
+    {
+        trackIR = trackIRRoot.GetComponent<TrackIRComponent>();
     }
 
     void Update()
