0214cc28c427f116430dae60b14101a409804116
ElliotOusley <ousleye@oregonstate.edu>
2025-11-19 20:46:02 -0800

    Added a timer with functions that can be accessed elsewhere in the game, made functions for health and score public, and added labels to the inspector

---
+++ Assets/Scripts/ManageUI.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using TMPro; //Using this to update text
+
+
+
+
+public class ManageUI : MonoBehaviour
+{ 
+
+    // Audio Sources
+    [Header("Audio")]
+    public AudioSource scoreAudio; // Listing them like this so only one header is displayed in the editor
+    public AudioSource healthGainAudio, healthLostAudio;
+    
+
+    // Health Variables
+    [Header("Health  Variables")]
+    public TMP_Text healthObject;
+    public RectTransform healthBarObject, healthBarObjectFill;
+    public int maxHealth = 100;
+
+    [HideInInspector] // Can be used by other scripts, but doesnt show up
+    private int health = 0;
+
+    // Score Variables
+    [Header("Score Variables")]
+    public TMP_Text scoreObject;
+    [HideInInspector]
+    public int score = 0;
+
+    // Timer Variables
+    [Header("Timer Variables")]
+    public TMP_Text timerObject;
+
+    [HideInInspector]
+    public float timer = 0f;
+
+    private bool timerRunning = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerRunning)
+        {
+            timer += Time.deltaTime;
+            UpdateTimerUI(); // Done through a function so the timer can be started, stopped, or reset elsewhere
+        }
+    }
+
+
+    public void ChangeHealth(int amount)
+    {
+        health += amount;
+
+        if (health <= 0)
+        {
+            print("HEALTH AT 0. ACT ON IT HERE");
+        }
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        if (amount > 0) // Play the sound for gaining health
+        {
+            healthGainAudio.Play(0);
+        }
+        else if (amount < 0) // Play the sound for losing health
+        {
+            healthLostAudio.Play(0);
+        }
+
+
+        string healthString = "Health: " + health + "/" + maxHealth;
+
+        healthObject.text = healthString;
+
+
+
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+
+        float healthPercent = (float)health / (float)maxHealth;
+
+        Vector2 size = healthBarObjectFill.sizeDelta;
+        size.y = healthBarObject.sizeDelta.y * healthPercent;
+        healthBarObjectFill.sizeDelta = size;
+    }
+
+    // Scoring Functions
+
+    public void ChangeScore(int amount)
+    {
+        score += amount;
+
+        scoreAudio.Play(0);
+
+        string scoreString = "Score: " + score;
+        scoreObject.text = scoreString;
+    }
+
+    // Timer Functions
+
+    private void UpdateTimerUI() // Keeping private because this is the only script that should really need it.
+    {
+        if (timerObject != null)
+        {
+            timerObject.text = "Time: " + timer.ToString("0.00");
+        }
+    }
+
+    // Starts the timer
+    public void StartTimer()
+    {
+        timerRunning = true;
+        print("Starting Timer");
+    }
+
+    // Stops the timer
+    public void StopTimer()
+    {
+        timerRunning = false;
+        print("Stopping Timer");
+    }
+
+    // Resets the timer and turns it off
+    public void ResetTimer()
+    {
+        timer = 0f;
+        StopTimer();
+        UpdateTimerUI();
+    }
+
+    // Resets the timer while keeping it going
+    public void RestartTimer()
+    {
+        timer = 0f;
+        StartTimer();
+        UpdateTimerUI();
+    }
+
+
+}
