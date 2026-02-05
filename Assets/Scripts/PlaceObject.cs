0926f9a022643e72bb21fa91f3ace90f8559cb12
Bopittostart <qtroth2004@gmail.com>
2026-02-04 20:59:20 -0800

    Created a new city with empty game objects placed that each spawn a random building at their location.

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -3,31 +3,27 @@ using UnityEngine;
 public class PlaceObject : MonoBehaviour
 {
     public GameObject[] prefabs; // List of buildings to place
-    [Range(0f, 1f)] public float placeChance = 0.1f; // Should be fairly low
-    [Range(0f, 5f)] public float randomOffset = 0.1f; // Shifts objects around a bit
-    [Range(-5f, 5f)] public float yOffset = 1f; // Y offset if buildings are in tiles
+    private float placeChance = 0.9f; // Should be fairly high
+    private float randomOffset = 0.1f; // Shifts objects around a bit
 
-    public void place_object()
+    public void Start()
     {
-        foreach (Transform child in transform)
+        if (prefabs == null || prefabs.Length == 0)
         {
-            if (!child.CompareTag("BuildingTile")) // Check to make sure that tile has the right tag (not the road)
-                continue;
-
-            if (Random.value <= placeChance)
-            {
-                GameObject prefab = prefabs[Random.Range(0, prefabs.Length)]; // Get prefab from the list
-
-                // Add a random x and z offset for some variation in building position
-                Vector3 randomPos = new Vector3(Random.Range(-randomOffset, randomOffset), 0f, Random.Range(-randomOffset, randomOffset));
-
-
-                Vector3 objectPos = child.position + Vector3.up * yOffset + randomPos; // Position of building, based on tile position
-
-
-                GameObject placedObject = Instantiate(prefab, objectPos, Quaternion.identity); // Place the building
-                placedObject.transform.SetParent(child, true); // Set the new object as a child of the level tile
-            }
+            return;
         }
+
+        if (Random.value >= placeChance)
+        {
+            return;
+        }
+        // Get a random prefab from the list
+        GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+        // Get the postion and apply random offsets
+        Vector3 position = transform.position;
+        position.x += Random.Range(-randomOffset, randomOffset);
+        position.z += Random.Range(-randomOffset, randomOffset);
+
+        Instantiate(prefab, position, Quaternion.identity, transform);
     }
 }
