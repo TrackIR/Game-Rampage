e2f5ea0a69ef9f7c388b3871bba888a9dd0d6e03
Ryan Waltemate <waltemry@oregonstate.edu>
2026-02-05 00:28:41 -0800

    Saves play settings globaly game-wide

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Game Settings")]
+public class GameSettings : ScriptableObject
+{
+    public string difficulty;
+    public string mapIndex;
+    // add more settings as they appear
+}
