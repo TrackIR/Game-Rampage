c9d56dd5ed5da8b0ad26f209563aef0ea9d4c605
Pranav Kulkarni <pranav.kulkarni.004@gmail.com>
2026-05-28 12:57:21 -0700

    Added splash text below title

---
+++ Assets/Scripts/SplashBounce.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SplashBounce : MonoBehaviour
+{
+    private Vector3 originalScale;
+    public float bounceSpeed = 8f;     
+    public float bounceAmount = 0.1f;   
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        float scaleModifier = Mathf.Sin(Time.time * bounceSpeed) * bounceAmount;
+        transform.localScale = originalScale * (1f + scaleModifier);
+    }
+}
